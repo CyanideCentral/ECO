e5857ca4ce8af666e4f2f3c629f333e217240cae
magus <tetracyanoethylene@163.com>
2018-06-13 09:51:07 +0800

    Get init_rect

--- test_challenge.m
+++ test_challenge.m
@@ -8,6 +8,7 @@ setup_paths();
 % Load video information
 data_path = [getenv('HOME'), '/dataset/VisDrone Dataset/3 - Single-Object Tracking/VisDrone2018-SOT-test-challenge/'];
 video_path = [data_path, 'sequences/'];
+init_path = [data_path, 'annotations/'];
 list = dir(video_path);  
 disp(list);
 
@@ -15,7 +16,12 @@ for k = 3:size(list, 1)
     vid = list(k).name;
     frames_dir = [video_path, vid, '/'];
     frames_list = dir(frames_dir);
-    filenames = extractfield(frames_list(3:size(frames_list,1)), 'name');
+    seq.len = size(frames_list,1);
+    init_file = [init_path, vid, '.txt'];
+    seq.init_rect = fscanf(fopen(init_file), '%f,%f,%f,%f');
+    filenames = extractfield(frames_list(3:seq.len), 'name')';
+    seq.format = 'otb';
+    seq.s_frames = strcat(frames_dir, filenames);
 end
 
 % Run ECO
